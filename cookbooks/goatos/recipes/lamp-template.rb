8e923767fb4c75205b662dcdc9bc47c9cdfb8ec6
Medhamsh V <medhamsh@axelerant.com>
2014-09-05 11:26:43 +0530

    changing the wget and extract of lamp-tempalte to be idempotent

--- cookbooks/goatos/recipes/lamp-template.rb
+++ cookbooks/goatos/recipes/lamp-template.rb
@@ -7,10 +7,12 @@ end
 remote_file '/opt/goatos/.local/share/lxc/lamp-template/lamp-template.tar.gz' do
   source "https://s3.amazonaws.com/projspace/lamp-template.tar.gz"
   mode 0644
+  not_if do ::File.exists?('/opt/goatos/.local/share/lxc/lamp-template/lamp-template.tar.gz') end
 end
 
 execute "extract-template" do
-  comman "sudo tar --same-owner -xzf /opt/goatos/.local/share/lxc/lamp-template/lamp-template.tar.gz -C /opt/goatos/.local/share/lxc/lamp-template/"
+  command "sudo tar --same-owner -xzf /opt/goatos/.local/share/lxc/lamp-template/lamp-template.tar.gz -C /opt/goatos/.local/share/lxc/lamp-template/"
+  not_if do ::File.directory?('/opt/goatos/.local/share/lxc/lamp-template') end
 end
 
 goatos_lxc_config 'lamp-template' do
