6cd6847ed7049778ceb8c259e871ca274eaa2b6a
Medhamsh V <medhamsh@axelerant.com>
2014-09-06 15:49:43 +0530

    Implementing container create, start and display ip using mixlib

--- cookbooks/goatos/files/default/create_container.rb
+++ cookbooks/goatos/files/default/create_container.rb
@@ -39,57 +39,30 @@ class Container
     :show_options => true,
     :exit => 0
 
-  attr_accessor :name, :type, :memory, :cpus
-
-  def initialize(args)
-    @container_info = {
-      name: args[0],
-      type: args[1],
-      memory: args[2],
-      cpus: args[3],
-    }
-
-    container = new_container(@container_info[:type])
-    container.clone(@container_info[:name], flags: LXC::LXC_CLONE_SNAPSHOT, bdev_type: 'overlayfs' )
-  end
-
-  def new_container(param)
-    LXC::Container.new(param)
-  end
-
-  def create_and_start
-    @container = new_container(@container_info[:name])
-    @container.start
-    sleep(5)
-  end
-
-  def set_cgroup_limits
-    @container.set_cgroup_item("memory.limit_in_bytes", @container_info[:memory])
-    @container.set_cgroup_item("cpuset.cpus", @container_info[:cpus])
-  end
-
-  def attach
-    @container.execute do
-    #run custom commands inside containers
+    def init(argv=ARGV)
+      parse_options(argv)
+      puts config.inspect
+      container = new_container(config[:type])
+      container.clone(config[:name], flags: LXC::LXC_CLONE_SNAPSHOT, bdev_type:  'overlayfs' )
     end
+
+    def new_container(param)
+      LXC::Container.new(param)
+    end
+
+    def create_and_start
+      @container = new_container(config[:name])
+      @container.start
+      sleep(5)
+      @container.set_cgroup_item("memory.limit_in_bytes", config[:memory])
+      @container.set_cgroup_item("cpuset.cpus", config[:cpus])
+      sleep(5)
+      puts @container.ip_addresses
+    end
+
   end
 
-  def ips
-    @container.ip_addresses
-  end
+    container = Container.new
+    container.init
+    container.create_and_start
 
-end
-
-
-arguments = ARGV
-
-if arguments.length == 4
-  puts "You have passed correct number of arguments."
-  container = Container.new(arguments)
-  container.create_and_start
-  container.set_cgroup_limits
-  container.attach
-  puts container.ips
-else
-  puts "Please check the number of arguments passed, it should be four arguments maximum."
-end
