c567c973c00cd0584440ec38eabefb3186df9c0b
Ranjib Dey <ranjib@pagerduty.com>
2014-09-07 04:13:35 -0700

    add lxc create cli

--- lib/goatos/cli/lxc.rb
+++ lib/goatos/cli/lxc.rb
@@ -4,24 +4,55 @@ require 'blender/chef'
 module GoatOS
   class CLI < Thor
     class Lxc < Thor
-
-      desc 'lxc ls', 'list all lxc'
-      option :filter,
+      class_option :filter,
         aliases: '-f',
         default: '*:*',
         description: 'Search term to predicate host list'
-      option :attribute,
+      class_option :attribute,
         aliases: '-a',
         default: 'ipaddress',
         description: 'Node attribute to be used for ssh hostname'
 
+      desc 'lxc ls', 'list all lxc'
       def ls
         opts = options.dup
         run_blender('lxc-ls --fancy', opts)
       end
+
+      desc 'lxc create', 'create a container'
+      option :template,
+        default: 'download',
+        aliases: '-t',
+        description: 'Template for building rootfs'
+      option :arch,
+        default: 'amd64',
+        aliases: '-a',
+        description: 'ARCH for the lxc'
+      option :distro,
+        default: 'ubuntu',
+        aliases: '-d',
+        description: 'Disro type to be used with download template'
+      option :release,
+        default: 'trusty',
+        aliases: '-r',
+        description: 'Release of a distribution (e.g lucid, precise, trusty for ubuntu)'
+      option :name,
+        required: true,
+        aliases: '-N',
+        description: 'Name of the container'
+      def create
+        opts = options.dup
+        command =[ 'lxc-create']
+        command += ['-t', opts[:template]]
+        command += ['-n', opts[:name], '--']
+        command += ['-d', opts[:distro]]
+        command += ['-r', opts[:release]]
+        command += ['-a', opts[:arch]]
+
+        run_blender(command.join(' '), opts)
+      end
       no_commands do
         def run_blender(command, opts)
-          puts opts.inspect
           Blender.blend('goatos_lxc') do |sched|
             sched.config(:chef, config_file: 'etc/knife.rb', attribute: opts[:attribute])
             sched.config(:ruby, stdout: $stdout)
