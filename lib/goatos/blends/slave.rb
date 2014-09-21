f78893e6e6e4b1cc4466372de71b5b05c6aea6bf
Ranjib Dey <ranjib@pagerduty.com>
2014-09-21 02:40:37 -0700

    thrust

--- lib/goatos/blends/slave.rb
+++ lib/goatos/blends/slave.rb
@@ -4,22 +4,19 @@ module GoatOS
   module Blends
     module Slave
       def add_slave_tasks(sched, options )
-
         sched.ssh_task 'sudo apt-get update -y'
-
-        sched.ssh_task 'run chef' do
-          execute 'sudo chef-client --no-fork -o recipe[goatos::lxc_install]'
+        sched.ssh_task 'sudo chef-client --no-fork -o recipe[goatos::lxc_install]' do
+          ignore_failure true
         end
-
-        sched.ssh_task 'run chef' do
-          execute 'sudo chef-client --no-fork -o recipe[goatos::lxc_configure]'
+        sched.ssh_task 'sudo chef-client --no-fork -o recipe[goatos::lxc_configure]' do
+          ignore_failure true
         end
-
+        sched.ssh_task 'sudo chef-client --no-fork -o recipe[goatos::lxc_install]'
+        sched.ssh_task 'sudo chef-client --no-fork -o recipe[goatos::lxc_configure]'
         sched.ssh_task 'reboot instance' do
           execute 'sudo reboot'
           ignore_failure true
         end
-
         sched.ruby_task 'sleep 90s' do
           execute do |h|
             sleep 90
